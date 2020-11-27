a3cadeb540aac0ad8945679d38ea5eb220047536
ray <birken1018@gmail.com>
2020-11-28 02:44:12 +0800

    [feat] auth send confirmation email

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_mail import Mail
 
 from config import config
 
@@ -11,12 +12,14 @@ login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
+mail = Mail()
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
     db.init_app(app)
+    mail.init_app(app)
     login_manager.init_app(app)
     bootstrap.init_app(app)
 
